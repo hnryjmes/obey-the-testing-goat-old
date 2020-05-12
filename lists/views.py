93e8c8d41ca9e60aef189d3fd234d6a6188e9633
Henry Cooksley <hcooksley@deloitte.co.uk>
2020-05-12 10:19:54 +0100

    Update URLs for forms

--- lists/views.py
+++ lists/views.py
@@ -2,12 +2,11 @@ from django.shortcuts import redirect, render
 
 from lists.models import Item
 
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
 
-# Create your views here.
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world')
     return render(request, 'home.html')
 
 
